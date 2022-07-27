a62d95c60dd28b5fbfe8974813239dd77003c3cb
lijedi <lijedi@outlook.com>
2022-07-27 10:14:08 +0800

    修改了hanzi_chaizi/hanzi_chaizi/__init__.py文件的query()函数，实现的功能：对于输入不可拆的汉字，输出的结果集为其自身

--- hanzi_chaizi/__init__.py
+++ hanzi_chaizi/__init__.py
@@ -10,13 +10,25 @@ class HanziChaizi(object):
         with open(data_file, 'rb') as fd:
             self.data = pickle.load(fd)
 
+
+    # 若输入的拆字为 "一" 这种不可拆的汉字时
+    # 拆字结果以其自生为结果集
+    # eg: input: 一
+    #    output: ['一']
     def query(self, input_char, default=None):
         result = self.data.get(input_char, default)
-        return result[0]
+        if result == None:
+            return [input_char]
+        else:
+            return result[0]
 
 
 if __name__ == "__main__":
     hc = HanziChaizi()
-    result = hc.query('名')
+    result_1 = hc.query('名')
+    result_2 = hc.query('一')
+    result_3 = hc.query('之')
 
-    print(result)
+    print(result_1)
+    print(result_2)
+    print(result_3)
